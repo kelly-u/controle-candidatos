846056862607fd5bcd31ccb637a5de3c24e6cf4b
G. Kelly Macedo <gleicekellymacedodd@gmail.com>
2024-07-10 23:14:24 -0300

    Atualizando a classe Processo Seletivo para contratar os candidatos

--- src/ProcessoSeletivo.java
+++ src/ProcessoSeletivo.java
@@ -1,5 +1,7 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
     }
 
@@ -8,10 +10,23 @@ public class ProcessoSeletivo {
         
         int candidatosSelecionados = 0;
         int candidatosAtual = 0;
-        while (candidatosSelecionados < 5) {
+        double salarioBase = 2000.00;
+        while (candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
             String candidato = candidatos[candidatosAtual];
-            
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " solicitou este valor de salário: " + salarioPretendido);
+
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga.");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
         }
+    }
+
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
 
     static void analisarCandidato(double salarioPretendido){
